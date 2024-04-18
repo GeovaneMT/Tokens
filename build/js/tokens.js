00428cedc9b8e0e7aa4db9919f12868a40a68007
Geovane <gmtgeovane@gmail.com>
2024-04-18 11:23:31 -0300

    Tokens repo

---
+++ build/js/tokens.js
@@ -0,0 +1,8 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 18 Apr 2024 14:13:33 GMT
+ */
+
+module.exports = {
+  "GradientPipersGradient": {"gradientType":"linear","rotation":135,"stops":[{"position":0,"color":"#8badd0de"},{"position":0.52,"color":"#db516cff"},{"position":1,"color":"#f1983cff"}]}
+};
